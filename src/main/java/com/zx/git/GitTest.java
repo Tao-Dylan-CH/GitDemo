2ba5edf5878c5d103ebe4ca616dffa80a3dce316
dylan <3402747477@qq.com>
2022-08-14 17:19:16 +0800

    second commit

--- src/main/java/com/zx/git/GitTest.java
+++ src/main/java/com/zx/git/GitTest.java
@@ -9,5 +9,6 @@ package com.zx.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello Git2!");
     }
 }
