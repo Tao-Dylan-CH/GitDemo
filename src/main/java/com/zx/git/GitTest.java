829653fbe3bb2033b78388453d0192b94a37b2a9
dylan <3402747477@qq.com>
2022-08-14 17:15:19 +0800

    first commit

---
+++ src/main/java/com/zx/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.zx.git;
+
+/**
+ * @author 挚爱之夕
+ * @version 1.0
+ * @implSpec com.zx.git
+ * @since 2022 - 08 - 14 - 17:13
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
